18c6170dcd02bd2ac97360d37407f7ece89a4cee
José Alberto Rodrigues Neto <josealberto270920@gmail.com>
2023-05-22 22:45:51 -0300

    adding connection dependecies and configuring the server

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
+
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
+
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public')) // tornar qualquer arquivo dentro da pasta public, disponivel para o publico no
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log('server did load')
+
+
+// verificar a conexao com  servidor entre os 16 e 17 min, refazer essa parte da configuracao
