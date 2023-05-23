0b0293981e46ad119199a91a82c2eb28e3f90560
José Alberto Rodrigues Neto <josealberto270920@gmail.com>
2023-05-23 01:09:20 -0300

    create first player using sockets

--- app.js
+++ app.js
@@ -15,9 +15,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+const players = {}
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+  console.log('a user connected')
+  players[socket.id] = {
+    x: 100,
+    y: 100
+  }
+
+  io.emit('updatePlayers', players)
+
+  console.log(players)
+})
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
