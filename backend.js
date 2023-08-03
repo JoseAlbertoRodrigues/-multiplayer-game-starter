f2581a7033f226086d335482aa90dadda72d70c9
José Alberto Rodrigues Neto <josealberto270920@gmail.com>
2023-08-03 18:46:55 -0300

    Server reconciliation (fix any lag)

--- backend.js
+++ backend.js
@@ -24,7 +24,8 @@ io.on('connection', (socket) => {
   backEndPlayers[socket.id] = {
     x: 500 * Math.random(),
     y: 500 * Math.random(),
-    color: `hsl(${360 * Math.random()}, 100%, 50%)`
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`,
+    sequenceNumber: 0
   }
 
   io.emit('updatePlayers', backEndPlayers)
@@ -35,7 +36,8 @@ io.on('connection', (socket) => {
     io.emit('updatePlayers', backEndPlayers)
   })
 
-  socket.on('keydown', (keycode) => {
+  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+    backEndPlayers[socket.id].sequenceNumber = sequenceNumber
     switch (keycode) {
       case 'KeyW':
         backEndPlayers[socket.id].y -= SPEED
@@ -57,13 +59,10 @@ io.on('connection', (socket) => {
 
 setInterval(() => {
   io.emit('updatePlayers', backEndPlayers)
-}, 1500)
+}, 15)
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 console.log('server did load')
-
-
-// verificar a conexao com  servidor entre os 16 e 17 min, refazer essa parte da configuracao
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -28,9 +28,29 @@ socket.on('updatePlayers', (backEndPlayers) => {
         color: backEndPlayer.color
       })
     } else {
-      // if a player already exists
-      frontEndPlayers[id].x = backEndPlayer.x
-      frontEndPlayers[id].y = backEndPlayer.y
+      if ( id === socket.id ) {
+        // if a player already exists
+        frontEndPlayers[id].x = backEndPlayer.x
+        frontEndPlayers[id].y = backEndPlayer.y
+
+        const lastBackendInputIndex = playerInputs.findIndex(input => {
+          return backEndPlayer.sequenceNumber === input.sequenceNumber
+        })
+
+        if (lastBackendInputIndex > -1)
+          playerInputs.splice(0, lastBackendInputIndex + 1)
+
+        playerInputs.forEach((input) => {
+          frontEndPlayers[id].x += input.dx
+          frontEndPlayers[id].y += input.dy
+        })
+      } else {
+        // for all other players
+        frontEndPlayers[id].x = backEndPlayer.x
+        frontEndPlayers[id].y = backEndPlayer.y
+      }
+
+
     }
   }
 
@@ -194,25 +214,36 @@ const keys = {
 }
 
 const SPEED = 10
+const playerInputs = []
+let sequenceNumber = 0
 setInterval(() => {
   if (keys.w.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: 0, dy: -SPEED })
     frontEndPlayers[socket.id].y -= SPEED
-    socket.emit('keydown', 'KeyW')
+    socket.emit('keydown', { keycode: 'KeyW', sequenceNumber })
+
   }
 
   if (keys.a.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: -SPEED, dy: 0 })
     frontEndPlayers[socket.id].x -= SPEED
-      socket.emit('keydown', 'KeyA')
+      socket.emit('keydown', { keycode: 'KeyA', sequenceNumber })
   }
 
   if (keys.s.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: 0, dy: SPEED })
     frontEndPlayers[socket.id].y += SPEED
-    socket.emit('keydown', 'KeyS')
+    socket.emit('keydown', { keycode: 'KeyS', sequenceNumber })
   }
 
   if (keys.d.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: SPEED, dy: 0 })
     frontEndPlayers[socket.id].x += SPEED
-    socket.emit('keydown', 'KeyD')
+    socket.emit('keydown', { keycode: 'KeyD', sequenceNumber })
   }
 }, 1500)
 
